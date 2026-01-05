749cb313c17de4561fd43d368c6ce179afdc8a7c
RiyaChaudhari2005 <chaudhaririya910@gmail.com>
2026-01-05 16:45:09 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,5 +1,7 @@
 // Primitive
 
+const { useInsertionEffect } = require("react");
+
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
 const score = 100
@@ -33,3 +35,26 @@ const myFuction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "riyachaudharidotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "riya@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
